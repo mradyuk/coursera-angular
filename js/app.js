6cf9e808fd3c8c76d32340990369730a87b5cced
Maria Radziuk <mariaradyuk@gmail.com>
2017-03-13 14:52:47 +0300

    assignment 2

--- js/app.js
+++ js/app.js
@@ -1,29 +1,69 @@
 (function () {
-  'use strict';
-  angular.module('assignment1', []).controller('Assignment1Controller', Assignment1Controller);
 
-  Assignment1Controller.$inject = [$scope];
-  function Assignment1Controller($scope) {
+    'use strict';
 
-    //$scope.lunch ="1,2,3,4,5" ;
-    $scope.checkIfTooMuch = function () {
+    angular.module('ShoppingListApp', [])
+        .controller('ShoppingListBuyController', ShoppingListBuyController)
+        .controller('ShoppingListDoneController', ShoppingListDoneController)
+        .service('ShoppingListService', ShoppingListService);
 
-      if ($scope.lunch === "") {
-        $scope.message = "Please enter data first";
-      }
+    ShoppingListBuyController.$inject = ['ShoppingListService'];
+    ShoppingListDoneController.$inject = ['ShoppingListService'];
 
-      var dishes = $scope.lunch.split(',');
-      for (var i = 0; i < dishes.length; i++) {
-        if (dishes[i] === "") {
-          dishes.splice(i, 1);
-        }
-      }
 
-      if (dishes.length > 3) {
-        $scope.message = "Too much!";
-      }
+    function ShoppingListBuyController(ShoppingListService) {
+
+        var shoppingList = this;
+
+        shoppingList.listIsEmpty = false;
+
+        shoppingList.items = ShoppingListService.getToDoItems();
+
+
+        shoppingList.removeItem = function (itemIndex) {
+            ShoppingListService.removeItem(itemIndex);
+            if (shoppingList.items.length === 0) {
+                shoppingList.listIsEmpty = true;
+            }
+        };
+
     }
-  }
+
+    function ShoppingListDoneController(ShoppingListService) {
+
+        var doneList = this;
+        doneList.listIsEmpty = true;
+
+        doneList.items = ShoppingListService.getDoneItems();
+    }
+
+    function ShoppingListService() {
+        var service = this;
+
+        // List of shopping items
+        var todo = [
+            { name: "cookies", quantity: 10 },
+            { name: "soda", quantity: 1 },
+            { name: "cake", quantity: 3 },
+            { name: "spagetti", quantity: 5 },
+            { name: "avocado", quantity: 10 }
+        ];
+
+        var done = [];
+
+        service.removeItem = function (itemIdex) {
+            var deleted = todo.splice(itemIdex, 1);
+            Array.prototype.push.apply(done, deleted);
+        };
+
+        service.getToDoItems = function () {
+            return todo;
+        };
+
+        service.getDoneItems = function () {
+            return done;
+        };
+    }
 
 
 })();
